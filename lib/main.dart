568b838fbae0e664f601492ea9c0dbe1bc50270a
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-02-01 09:28:31 +0200

    change theme and create notes view

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/notes_view.dart';
 
 void main() {
   runApp(const NotesApp());
 }
+
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home:const NotesView(),
+    );
   }
 }
