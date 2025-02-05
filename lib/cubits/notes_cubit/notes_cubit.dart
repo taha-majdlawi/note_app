e390b11e8493f8fc818b92c13f02128114e1ce6f
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-02-05 20:31:14 +0200

    create fetsh note method

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:note_app/constents.dart';
 import 'package:note_app/models/note_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
-}
+
+  fetchAllNotes() async {
+   
+    try {
+      var noteBox = Hive.box<NoteModel>(kBoxName);
+     List<NoteModel> notes = await noteBox.values.toList();
+      await Future.delayed(Duration(milliseconds: 300));
+      emit(NotesSuccess(notes));
+    } on Exception catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
+  }
+
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:note_app/widgets/add_note_bottom_sheet.dart';
 import 'package:note_app/widgets/custome_textfeild.dart';
 import 'package:note_app/widgets/notes_view_body_widget.dart';
@@ -8,20 +10,23 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: NotesViewBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            context: context,
-            builder: (context) {
-              return AddNoteBottomSheet();
-            },
-          );
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.blueAccent,
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        body: NotesViewBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              context: context,
+              builder: (context) {
+                return AddNoteBottomSheet();
+              },
+            );
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.blueAccent,
+        ),
       ),
     );
   }
