0935c81e4c583a67ec4ce4870db18d9aab68ddfa
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-02-05 10:30:44 +0200

    create cubit

---
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -0,0 +1,10 @@
+part of 'add_note_cubit.dart';
+
+sealed class AddNoteState extends Equatable {
+  const AddNoteState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class AddNoteInitial extends AddNoteState {}
